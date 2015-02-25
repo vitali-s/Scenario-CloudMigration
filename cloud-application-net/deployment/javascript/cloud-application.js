e000c9cc203542f7c19eabb20c2a0319a7b5425a
unknown <Vitali_Shcharbin@EPBYMINW0989.minsk.epam.com>
2015-02-25 16:03:25 +0300

    Add deployment package for .net application

---
+++ cloud-application-net/deployment/javascript/cloud-application.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    angular
+        .module("cloud-application", ["ngRoute"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/content", {
+                templateUrl: "/views/content.html"
+            });
+
+            $routeProvider.when("/database", {
+                templateUrl: "/views/database.html"
+            });
+
+            $routeProvider.when("/message-queue", {
+                templateUrl: "/views/message-queue.html"
+            });
+
+            $routeProvider.when("/mail", {
+                templateUrl: "/views/mail.html"
+            });
+
+            $routeProvider.otherwise({
+                templateUrl: "/views/application.html"
+            });
+        });
+})();
